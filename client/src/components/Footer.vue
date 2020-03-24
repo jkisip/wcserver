e6c72d2e5b7a7a685f22f92aa5784fba0751d26f
Jan Kristopher Isip <jkisipp@gmail.com>
2020-03-24 20:07:16 +0800

    First commit

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,220 @@
+<template>
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <h6>About</h6>
+            <p class="text-justify">Suspendisse vitae mauris quis lacus posuere viverra. Mauris sit amet nunc erat. Aliquam sit amet lorem sit amet metus cursus feugiat. Fusce luctus imperdiet fermentum. Etiam id euismod elit, bibendum eleifend nunc. Nullam non tincidunt velit. Vestibulum dapibus diam et neque pulvinar, at aliquet justo auctor. Sed nec venenatis massa. Curabitur facilisis, lectus at cursus placerat, felis leo fringilla elit, a sagittis ligula nisi bibendum dui. Aliquam convallis magna ut sagittis ultrices. Nunc egestas rutrum elit, in pulvinar magna efficitur sodales. Nulla eu molestie augue, finibus consequat diam. Aliquam erat volutpat. Suspendisse faucibus ornare purus in vulputate. Proin dapibus dapibus lacinia. Nam dui est, suscipit a consequat eget, auctor eu magna.</p>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Useful Links</h6>
+            <ul class="footer-links">
+              <li><a href="http://scanfcode.com/category/c-language/">Daily News</a></li>
+              <li><a href="http://scanfcode.com/category/front-end-development/">Top News</a></li>
+              <li><a href="http://scanfcode.com/category/back-end-development/">Important Dates</a></li>
+              <li><a href="http://scanfcode.com/category/java-programming-language/"></a>Members</li>
+              <li><a href="http://scanfcode.com/category/android/">Android</a></li>
+              <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
+            </ul>
+          </div>
+
+          <div class="col-xs-6 col-md-3">
+            <h6>Contact</h6>
+            <ul class="footer-links">
+              <li style="font-weight:bold;"><a href="http://scanfcode.com/about/">Mailing Address</a></li>
+              <li><a href="http://scanfcode.com/contact/">WORLD News</a></li>
+              <li><a href="http://scanfcode.com/contribute-at-scanfcode/">6781 LA HWY 24</a></li>
+              <li><a href="http://scanfcode.com/privacy-policy/">(785) 562-9000</a></li>
+              <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by 
+         <a href="#">Scanfcode</a>.
+            </p>
+          </div>
+
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons">
+              <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+              <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
+            </ul>
+          </div>
+        </div>
+      </div>
+</footer>
+</template>
+
+<style>
+.site-footer
+{
+  background-color:#26272b;
+  padding:45px 0 20px;
+  font-size:15px;
+  line-height:24px;
+  color:#737373;
+}
+.site-footer hr
+{
+  border-top-color:#bbb;
+  opacity:0.5
+}
+.site-footer hr.small
+{
+  margin:20px 0
+}
+.site-footer h6
+{
+  color:#fff;
+  font-size:16px;
+  text-transform:uppercase;
+  margin-top:5px;
+  letter-spacing:2px
+}
+.site-footer a
+{
+  color:#737373;
+}
+.site-footer a:hover
+{
+  color:#3366cc;
+  text-decoration:none;
+}
+.footer-links
+{
+  padding-left:0;
+  list-style:none
+}
+.footer-links li
+{
+  display:block
+}
+.footer-links a
+{
+  color:#737373
+}
+.footer-links a:active,.footer-links a:focus,.footer-links a:hover
+{
+  color:#3366cc;
+  text-decoration:none;
+}
+.footer-links.inline li
+{
+  display:inline-block
+}
+.site-footer .social-icons
+{
+  text-align:right
+}
+.site-footer .social-icons a
+{
+  width:40px;
+  height:40px;
+  line-height:40px;
+  margin-left:6px;
+  margin-right:0;
+  border-radius:100%;
+  background-color:#33353d
+}
+.copyright-text
+{
+  margin:0
+}
+@media (max-width:991px)
+{
+  .site-footer [class^=col-]
+  {
+    margin-bottom:30px
+  }
+}
+@media (max-width:767px)
+{
+  .site-footer
+  {
+    padding-bottom:0
+  }
+  .site-footer .copyright-text,.site-footer .social-icons
+  {
+    text-align:center
+  }
+}
+.social-icons
+{
+  padding-left:0;
+  margin-bottom:0;
+  list-style:none
+}
+.social-icons li
+{
+  display:inline-block;
+  margin-bottom:4px
+}
+.social-icons li.title
+{
+  margin-right:15px;
+  text-transform:uppercase;
+  color:#96a2b2;
+  font-weight:700;
+  font-size:13px
+}
+.social-icons a{
+  background-color:#eceeef;
+  color:#818a91;
+  font-size:16px;
+  display:inline-block;
+  line-height:44px;
+  width:44px;
+  height:44px;
+  text-align:center;
+  margin-right:8px;
+  border-radius:100%;
+  -webkit-transition:all .2s linear;
+  -o-transition:all .2s linear;
+  transition:all .2s linear
+}
+.social-icons a:active,.social-icons a:focus,.social-icons a:hover
+{
+  color:#fff;
+  background-color:#29aafe
+}
+.social-icons.size-sm a
+{
+  line-height:34px;
+  height:34px;
+  width:34px;
+  font-size:14px
+}
+.social-icons a.facebook:hover
+{
+  background-color:#3b5998
+}
+.social-icons a.twitter:hover
+{
+  background-color:#00aced
+}
+.social-icons a.linkedin:hover
+{
+  background-color:#007bb6
+}
+.social-icons a.dribbble:hover
+{
+  background-color:#ea4c89
+}
+@media (max-width:767px)
+{
+  .social-icons li.title
+  {
+    display:block;
+    margin-right:0;
+    font-weight:600
+  }
+}
+</style>
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <!--<a class="navbar-brand" href="#">ABC NEWS</a>-->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link">
+        <router-link to="/">
+         Home
+        </router-link>
+        </a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link">
+        <router-link to="/Members">
+         Members
+        </router-link>
+          </a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link">
+        <router-link to="/News">
+         News
+        </router-link>
+          </a>
+      </li>
+    </ul>
+    <form class="search">
+      <input class="aaa" type="text" placeholder="Search" aria-label="Search">
+      <button class="aaa" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+</template>
+<style>
+.nav-link{
+  float:left;
+  margin-left: 390px;
+  font-size: 30px;
+}
+.router-link{
+  color: #35530a;
+}
+.aaa{
+  background-color:lightgray;
+}
+</style>
